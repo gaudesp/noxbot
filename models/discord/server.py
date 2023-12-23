71c5adeeebd2b6ada5c9fe25d768022c34ba7afd
vdesplanque <47974284+vdesplanque@users.noreply.github.com>
2023-12-23 22:51:49 +0100

    Update server.py

--- models/discord/server.py
+++ models/discord/server.py
@@ -5,10 +5,10 @@ class DiscordServer:
     self.storage = './db/discord/server.json'
     self.data = File(self.storage).read()
 
-  //tu peux utiliser la syntaxe "ternaire" :
-  // return value_if_true if condition else value_if_false
-  // ce qui donne : 
-  // return [] if self.data['list'] is None else self.data['list']
+  #tu peux utiliser la syntaxe "ternaire" :
+  # return value_if_true if condition else value_if_false
+  # ce qui donne : 
+  # return [] if self.data['list'] is None else self.data['list']
   def find_all(self):
     if self.data['list'] is None:
       return []
