d2a451b013aa26ea5f314f4681626a92c45d6886
gaudesp <gaudespro@gmail.com>
2024-11-01 13:41:09 +0100

    fix: set return type

--- src/cogs/guild.py
+++ src/cogs/guild.py
@@ -24,7 +24,7 @@ class GuildCog(commands.Cog):
     await self.guild_service.delete_guild(guild.id)
     self.bot.log(f"Guild {guild.name} (ID: {guild.id}) has removed {self.bot_name} to its server", "discord.on_guild_remove")
 
-  async def locale_autocomplete(self, interaction: discord.Interaction, current: str) -> None:
+  async def guild_locale_autocomplete(self, interaction: discord.Interaction, current: str) -> list[app_commands.Choice]:
     """Fournit des suggestions d'autocomplétion pour la locale."""
     await interaction.response.defer(thinking=True)
     locales = self.bot.i18n.get_locales()
@@ -34,7 +34,7 @@ class GuildCog(commands.Cog):
     ][:25] if locales else []
 
   @app_commands.command(name='nx_lang', description='placeholder')
-  @app_commands.autocomplete(locale=locale_autocomplete)
+  @app_commands.autocomplete(locale=guild_locale_autocomplete)
   @app_commands.checks.has_permissions(administrator=True)
   async def define_locale(self, interaction: discord.Interaction, locale: str) -> None:
     """Met à jour la langue de la guilde uniquement si la locale est valide."""
